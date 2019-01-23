9f7f674f34a0c252f9baadd66ab4f45de0f3e530
dgliwka <dawid.gliwka@gmail.com>
2019-01-23 15:00:58 +0100

    add link to .dotfiles

---
+++ link.sh
@@ -0,0 +1,5 @@
+ln -s ~/.dotfiles/.ngrok2 ~/
+ln -s ~/.dotfiles/omf/ ~/.config/
+ln -s ~/.dotfiles/fish/ ~/.config/
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/.tmux.conf.local ~/.tmux.conf.local
