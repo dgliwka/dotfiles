f527ed475e26f6148cd5948716a341c1b3253945
dgliwka <dawid.gliwka@gmail.com>
2019-04-30 23:52:52 +0200

    add monitoring containers (influxdb and grafana)

--- scripts/instaloader.sh
+++ scripts/instaloader.sh
@@ -4,6 +4,6 @@ cd /home/pi/Desktop/insta
 echo "start" >> /home/pi/.instaloader/log.txt
 date >> /home/pi/.instaloader/log.txt
 /usr/local/bin/instaloader --sessionfile=/home/pi/.instaloader/session-chujowydesign -l chujowydesign -s --highlights -F --no-captions --no-metadata-json fobiasloneczna belle.delphine szczupakerror pantyhose._.queen monikatyburska
-/usr/local/bin/instaloader -l dawid.gliwka --sessionfile=/home/pi/.instaloader/session-dawid.gliwka -s --highlights -F --no-captions --no-metadata-json kiro.jpg monik_nuda lovely.bananassa
+/usr/local/bin/instaloader --sessionfile=/home/pi/.instaloader/session-dawid.gliwka -l dawid.gliwka -s --highlights -F --no-captions --no-metadata-json kiro.jpg monik_nuda lovely.bananassa
 echo "done" >> /home/pi/.instaloader/log.txt
 date >> /home/pi/.instaloader/log.txt
