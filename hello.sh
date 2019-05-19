c8765a81e284f5cbc39518681413f9880d11a104
dgliwka <dawid.gliwka@gmail.com>
2019-05-19 18:58:15 +0200

    add fish and omf configuration

--- hello.sh
+++ hello.sh
@@ -1,9 +1,11 @@
 
-#install fish
-sudo apt-get -y install fish vim tmux
+#install fish and other utilites
+sudo apt-get -y install fish vim tmux mc
 
 #install omf
 curl -L https://get.oh-my.fish | fish
 
 #install omf git plugin
-omf install https://github.com/jhillyerd/plugin-git
+fish omf install https://github.com/jhillyerd/plugin-git
+fish omf install https://github.com/oh-my-fish/plugin-bang-bang
+fish omf install https://github.com/oh-my-fish/theme-taktoa
