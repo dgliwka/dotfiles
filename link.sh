731f544d12b89cdabcbbd04892068147cc6a0b3c
dgliwka <dawid.gliwka@gmail.com>
2020-03-19 00:17:40 +0100

    Add htop config

--- link.sh
+++ link.sh
@@ -2,3 +2,4 @@ ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.tmux.conf.local ~/.tmux.conf.local
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/htoprc ~/.config/htop/htoprc
