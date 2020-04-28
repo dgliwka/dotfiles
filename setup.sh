5eff707fab138b5cca6579da74afab10a2e64cfd
dgliwka <dawid.gliwka@gmail.com>
2020-04-29 00:07:43 +0200

    Add installing git, htop, tmux, zsh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+apt-get install git
 git clone https://github.com/ohmyzsh/ohmyzsh.git .oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions .oh-my-zsh/custom/plugins/zsh-autosuggestions
 ./dotfiles link
+apt-get install zsh htop tmux
