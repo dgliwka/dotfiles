37832e68c7331c10eba26ad60592c717ad6d2432
dgliwka <dawid.gliwka@gmail.com>
2020-05-03 18:42:44 +0200

    Get rid of gitmodules

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 apt-get install git
 git clone https://github.com/ohmyzsh/ohmyzsh.git .oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions .oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git .oh-my-zsh/custom/plugins/zsh-syntax-hightlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git .oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 ./dotfiles link
 apt-get install zsh htop tmux
