d85958138dc466167fc9487501381d811a707349
Dawid Gliwka <dawid.gliwka@gmail.com>
2020-05-12 00:52:58 +0200

    Get rid of zsh-npm plugin

--- setup.sh
+++ setup.sh
@@ -3,6 +3,5 @@ git clone https://github.com/ohmyzsh/ohmyzsh.git oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git oh-my-zsh/custom/themes/powerlevel10k
-git clone https://github.com/lukechilds/zsh-nvm oh-my-zsh/custom/plugins/zsh-nvm
 ./dotfiles link
 apt-get install zsh htop tmux
